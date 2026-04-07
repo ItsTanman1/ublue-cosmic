5bee40bb5ac1522d8ecfe3ebe0cbbd753f20f7c1
ItsTanman1 <tannerww2003@gmail.com>
2026-04-07 14:22:27 -0400

    Create kernel-cachyos.sh

---
+++ files/scripts/kernel-cachyos.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+#Remove Fedora kernel and cleanup files
+dnf -y remove kernel kernel-*
+rm -rf /usr/lib/modules/*
+
+#Enable the COPR repository
+dnf -y copr enable bieszczaders/kernel-cachyos
+
+#Install the CachyOS kernel and bypass scripts
+dnf -y install kernel-cachyos --setopt=tsflags=noscripts
+
+#Build modules, run depmod & generate initramfs
+VER=$(ls /lib/modules)
+depmod -a "$VER"
+dracut --kver "$VER" \
+    --force \
+    --add ostree \
+    --no-hostonly \
+    --reproducible \
+    "/usr/lib/modules/$VER/initramfs.img"
+
+#Cleanup and remove COPR repo
+dnf clean all
+rm -f /etc/yum.repos.d/*copr*.repo
